061341565da341cea0cd6c777895752df6b88e68
Vlad <raznoschikov.vlad@gmail.com>
2021-02-01 20:02:30 +0800

    9. Palindrome Number (easy)

---
+++ 9. Palindrome Number (easy)/revert-half.ts
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=9 lang=typescript
+ *
+ * [9] Palindrome Number
+ */
+
+// @lc code=start
+function isPalindrome(x: number): boolean {
+  if (x < 0 || (x % 10 === 0 && x !== 0)) return false;
+  
+  let reversed = 0;
+  while (x > reversed) {
+    reversed = (reversed * 10) + (x % 10);
+    x = Math.floor(x / 10);
+  }
+  
+  return x === reversed || x === Math.floor(reversed / 10);
+};
+// @lc code=end
+
